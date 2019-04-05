5fd6dc2ae04d2f60887ab3d0ff3cf1430d61b72a
Eli Kramer <eliyohukramer1311dk@gmail.com>
2019-04-04 21:25:16 -0400

    all

---
+++ Python/Homework37/PythonHomework37.py
@@ -0,0 +1,9 @@
+name = "Eliyohu Kramer"
+address = "959 West Kennedy Blvd."
+siblings = ['Yisroel', 'Shmuel', 'Moshe', 'Yehuda', 'Nosson', 'Aryeh']
+
+print(f'Full Name: {name}, Address: {address}, Siblings: {siblings}')
+
+print(name [::3])
+
+print(siblings[-2][1:-1])
