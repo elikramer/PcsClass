4c6615a57c349956b391416f97f1bff28513670e
Eli Kramer <eliyohukramer1311dk@gmail.com>
2019-06-23 21:13:09 -0400

    add file

---
+++ JavaScript/Homework54/54.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function everyArray_Function(array, callback){
+    for(let i = 0; let < array1.length; i++){
+        if(callback(theArray[i]) != true){
+            return false;
+        }
+    }
+    return true;
+}
+
+function test (letter){
+    let pass = false;
+    if(letter === letter.toUpperCase()){
+        pass = true;
+    }
+    else{
+        pass = false;
+    }
+}
